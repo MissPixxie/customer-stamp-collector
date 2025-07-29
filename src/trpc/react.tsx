35a76c7b807a9aaf1c3f0b1c7868e3988b437c75
Emelie Hansson <emeliehanssonn@hotmail.se>
2025-07-29 20:51:41 +0200

    debugging

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -74,7 +74,7 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
 function getBaseUrl() {
   if (typeof window !== "undefined") return window.location.origin;
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-  if (process.env.DATABASE_URL) return `${process.env.DATABASE_URL}`;
+  if (process.env.DATABASE_URL) return `https://${process.env.DATABASE_URL}`;
   //return `http://localhost:${process.env.PORT ?? 3000}`;
   return `https://customer-stamp-collector-production.up.railway.app`;
 }
